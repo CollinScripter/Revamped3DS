8791fff2a48f79c91e6a6bcd9f008e5757b37ab8
CollinScripter <collinscripter@gmail.com>
2017-08-02 20:14:02 -0400

    Fixes #2

--- id1/solitude-z/textencoding.h
+++
@@ -1,8 +0,0 @@
-// textencoding.h
-
-wchar *decode_string (const char *s);
-char *encode_say (wchar *in);
-
-// Make sure the renderer can display all Unicode chars in the string,
-// otherwise try to replace them with Latin equivalents
-wchar *maybe_transliterate (wchar *src);
